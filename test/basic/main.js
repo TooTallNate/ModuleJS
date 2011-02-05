060f2829867421bd80ceebec4268a0859a93a8b5
Nathan Rajlich <nathan@tootallnate.net>
2011-02-04 22:12:54 -0800

    Set the title on all tests.

--- test/basic/main.js
+++ test/basic/main.js
@@ -1,3 +1,5 @@
+module.main.exports.setTitle("Basic Dependencies");
+
 module.load('./one', './two', function(one, two) {
   console.log(one.another);
   console.log('two.works:', two.works);
--- test/directory-default-index/main.js
+++ test/directory-default-index/main.js
@@ -1,7 +1,8 @@
+module.main.exports.setTitle('Directory Defaults to "index.js"');
+
 // When a dependency is specified with a '/' at the end, it is
 // assumed to be a directory name. So the dependency loaded
 // should be "$DIRNAME/index.js".
-
 module.load('./test/', function(testDir) {
   console.log(testDir);
 });
