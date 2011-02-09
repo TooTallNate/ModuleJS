c118a556e691b05eec9d5c5c91b23f54075cd177
Nathan Rajlich <nathan@tootallnate.net>
2011-02-09 12:42:56 -0800

    Added a test case to demonstrate interaction with the DOM.

---
+++ test/DOM/DOM.js
@@ -0,0 +1,32 @@
+// This module takes abstracts the global 'document' instance
+// into it's own classes/functions. This is very barebones, for
+// testing/demonstration purposes, but you can see how something
+// like this can be expanded on.
+
+
+// Get an instance to the global 'document'.
+var d = module.main.exports.document;
+
+
+// The 'Element' class is a wrapper around 'document.createElement()'
+function Element(type) {
+  console.log("creating a <"+type+">");
+  this.type = type;
+  this.ele = d.createElement(type);
+  console.log(this.ele);
+}
+
+Element.prototype.update = function(d) {
+  return (this.ele.innerHTML = d);
+}
+
+exports.Element = Element;
+
+
+// The 'insert' function adds an 'Element' instance into the global
+// scope's 'document.body'.
+
+exports.insert = function insert(element) {
+  console.log("appending <"+element.type+"> to <body>");
+  return d.body.appendChild(element.ele);
+}
---
+++ test/DOM/main.js
@@ -0,0 +1,18 @@
+// This is a simple test that is meant to demonstrate the ability
+// interact with the 'document' instance from the global scope.
+//
+// Thus, the line:  `exports.document = document;`
+// or something similar should be done in your main module.
+
+module.load('./DOM', function(DOM) {
+
+  // A wrapper for 'document.createElement()'
+  var foo = new (DOM.Element)('h1');
+
+  // Sets the 'innerHTML'
+  foo.update('It Works! <a href="..">Go Back</a>');
+
+  // Appends to the <body> tag
+  DOM.insert(foo);
+
+});
